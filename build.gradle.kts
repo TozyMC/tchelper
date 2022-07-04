ed907ef0317c1ff93dc43beb767054659b2c7471
TozyMC <tozymc@gmail.com>
2022-07-04 18:47:37 +0700

    Configure build.gradle.kts for multi-project purposes
    Set version to 0.0.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,29 @@
 plugins {
-    id("java-library")
+    id("java")
 }
 
 group = "com.github.tozymc"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
-dependencies {
-}
 
-val targetJavaVersion = 17
-java {
-    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
-    sourceCompatibility = javaVersion
-    targetCompatibility = javaVersion
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
-        vendor.set(JvmVendorSpec.AZUL)
+subprojects {
+    apply(plugin = "java-libary")
+
+    dependencies {
     }
-}
 
-tasks.withType<JavaCompile>().configureEach {
-    options.release.set(targetJavaVersion)
+    val targetJavaVersion = 17
+    java {
+        val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+            vendor.set(JvmVendorSpec.AZUL)
+        }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.release.set(targetJavaVersion)
+    }
 }
