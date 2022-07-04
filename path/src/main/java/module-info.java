29c41464632ac1fae58993124381c7ed3effc2ed
TozyMC <tozymc@gmail.com>
2022-07-04 19:06:49 +0700

    Add module-info.java

---
+++ path/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module tchelper.path {
+  requires static transitive org.jetbrains.annotations;
+
+  requires com.google.common;
+  requires org.slf4j;
+
+  exports com.github.tozymc.tchelper.path;
+}
